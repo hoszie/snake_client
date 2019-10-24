8a3e782ef7bff74ec992a160007befee09ef0af0
Nick <nhoszko@gmail.com>
2019-10-24 16:49:10 +0000

    connecting servers

---
+++ client.js
@@ -0,0 +1,17 @@
+// const connect = require('./play');
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '192.168.88.149',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  return conn;
+}
+
+module.exports = {
+  connect
+}
--- play.js
+++ play.js
@@ -1,19 +1,11 @@
-const net = require('net');
+// const net = require('net');
 
 /**
  * Establishes connection with the game server
  */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
 
-  return conn;
-}
-
+const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
 
+// module.exports = connect;
