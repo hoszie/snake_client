5e269ce47b4b2442624f9b3aaa71bfca3a78117e
Nick <nhoszko@gmail.com>
2019-10-25 22:37:28 +0000

    initials appear on snake

--- client.js
+++ client.js
@@ -1,21 +1,27 @@
-// const connect = require('./play');
 const net = require('net');
 
 const connect = function() {
   const conn = net.createConnection({ 
-    host: '192.168.88.149',
+    host: 'localhost',
     port: 50541
   });
-
-  conn.on('connect', () => {
-    console.log("Successfully connected to game server")
-  })
-  // interpret incoming data as text
+  
   conn.setEncoding('utf8'); 
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+  
+    conn.on('connect', () => {
+      console.log("Successfully connected to game server")
+    })
+
+    conn.on('connect', () => {   ///sending a name/snake initials
+      conn.write('Name: NPH');
+    });
+    // interpret incoming data as text
 
   return conn;
 }
-
 module.exports = {
   connect
 }
