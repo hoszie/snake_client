ffdf66a7ad7d3e9b17745756a900a7e517160207
Nick <nhoszko@gmail.com>
2019-10-26 00:35:18 +0000

    exporting ip and port from constants

--- client.js
+++ client.js
@@ -1,9 +1,11 @@
+const { IP, PORT } = require('./constants');
+
 const net = require('net');
 
 const connect = function() {
   const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
+    host: IP,
+    port: PORT
   });
   
   conn.setEncoding('utf8'); 
